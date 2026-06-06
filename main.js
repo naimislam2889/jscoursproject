12784308742b4759fc841412f2b0cd54011a2e67
Naim Islam <naimislam2889@gmail.com>
2026-06-06 08:06:56 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// 3a 
+console.log('3a - My name is:');
+
+// 3b
+console.log('3b - Naim');
+
+// 3c
+console.log('3c - My name is:Naim');
+
+// 3d
+console.log(`3d - Total cost: $${(1*5)+(1*3)}`);
+
+// 3e 
+console.log(`3e - Total cost: $${(1*5)+(1*3)}`);
+
+// 3f
+//alert(`3f - Total cost: $${(1*5)+(1*3)}`);
+
+// 3g 
+console.log(`3g - Total cost: $${(599+295)/100}`);
+
+// 3h 
+console.log(`3h - Total cost: $${(599+295)/100}`);
+
+// 3i 
+// alert((`3i - Total cost: $${(599+295)/100}`));
+
+// 3j 
+// alert(`3j - Total cost: $${(599+295)/100}\nThank you, come again!`) 
+
+// 3k
+console.log(`3k - Items (${2+2}): $${(2*2095+2*799)/100}`);
+
+// 3l
+console.log(`3l - Items (${2+2}): $${(2*2095+2*799)/100}\nShipping & handling: $${(2*499)/100}`);
+
+// 3m
+console.log(`3m - Items (${2+2}): $${(2*2095+2*799)/100}\nShipping & handling: $${(2*499)/100}\nTotal before tax: $${(5788+998)/100}`);
+
+// 3n
+console.log(`3n - Items (${2+2}): $${(2*2095+2*799)/100}\nShipping & handling: $${(2*499)/100}\nTotal before tax: $${(5788+998)/100}\nEstimated tax (10%): $${Math.round((6786*0.1))/100}`);
